ee11e6cdc2a2b1027ceba4e5e018f3f29848903d
EgorCheh <chehovich.egor@gmail.com>
2020-01-05 03:40:03 +0200

    added imageUploader

--- app/src/main/java/com/example/rx/AddPointActivity.java
+++ app/src/main/java/com/example/rx/AddPointActivity.java
@@ -1,24 +1,45 @@
 package com.example.rx;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AddPointActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
     private FirebaseFirestore db;
+    private ImageView imageView;
+
+    private Uri filePath;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,16 +48,15 @@ public class AddPointActivity extends AppCompatActivity {
         final EditText etDescription = findViewById(R.id.et_description);
         Button btnSetPoint = findViewById(R.id.set_new_point);
         db = FirebaseFirestore.getInstance();
-
-
-
+        imageView = findViewById(R.id.imageView);
+        openFileChooser();
+        
 
 
         btnSetPoint.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-
 
                 Map<String, Object> point = new HashMap<>();
                 GeoPoint geoPoint = new GeoPoint(56.5645,24.0623);
@@ -45,9 +65,6 @@ public class AddPointActivity extends AppCompatActivity {
                 point.put("gp", geoPoint);
                 point.put("description",description);
                 point.put("title", title);
-
-
-
 
                 db.collection("points")
                         .add(point)
@@ -63,7 +80,53 @@ public class AddPointActivity extends AppCompatActivity {
                                 Log.w("Firebase", "Error adding document", e);
                             }
                         });
+
             }
         });
+
     }
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+                uploadFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //Picasso.get().load(filePath).into(imageView);
+
+        }
+    }
+
+
+    private void uploadFile() {
+        StorageReference mStorageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference riversRef = mStorageRef.child("images/pic.jpg");
+        riversRef.putFile(filePath)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        //if the upload is successfull
+                        //hiding the progress dialog
+
+
+                        //and displaying a success toast
+                        Toast.makeText(getApplicationContext(), "File Uploaded ", Toast.LENGTH_LONG).show();
+                    }
+                });
+                  }
 }
--- app/src/main/java/com/example/rx/MainActivity.java
+++ app/src/main/java/com/example/rx/MainActivity.java
@@ -5,88 +5,43 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.GeoPoint;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.MapKitFactory;
-import com.yandex.mapkit.geometry.Point;
-import com.yandex.mapkit.location.Location;
-import com.yandex.mapkit.location.LocationListener;
-import com.yandex.mapkit.location.LocationManager;
-import com.yandex.mapkit.location.LocationStatus;
-import com.yandex.mapkit.map.CameraPosition;
-import com.yandex.mapkit.map.MapObject;
-import com.yandex.mapkit.map.MapObjectCollectionListener;
-import com.yandex.mapkit.map.MapObjectTapListener;
 import com.yandex.mapkit.mapview.MapView;
-import com.yandex.runtime.image.ImageProvider;
 
 
 public class MainActivity extends AppCompatActivity {
-    private MapView mapview;
+    private MapView mapView;
     private ImageButton buttonAdd;
     private FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MapKitFactory.setApiKey("f1b53a01-68ff-4b27-9d15-0f1738c860b3");
         MapKitFactory.initialize(this);
         setContentView(R.layout.activity_main);
-        mapview = findViewById(R.id.mapview);
+        mapView = findViewById(R.id.mapview);
         buttonAdd = findViewById(R.id.btn_add_point);
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),AddPointActivity.class));
+                startActivity(new Intent(getApplicationContext(), AddPointActivity.class));
             }
         });
         db = FirebaseFirestore.getInstance();
 
-        db.collection("points")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("Firebase", document.getId() + " => " + document.getData().get("gp"));
-                                GeoPoint geoPoint = (GeoPoint) document.getData().get("gp");
 
-                                mapview.getMap().getMapObjects().addPlacemark(
-                                        new Point(geoPoint.getLatitude(),geoPoint.getLongitude()),
-                                        ImageProvider.fromBitmap(getBitmapFromVectorDrawable(getApplicationContext(),R.drawable.ic_person_pin)));
-                            }
-                        } else {
-                            Log.w("Firebase", "Error getting documents.", task.getException());
-                        }
-                    }
-                });
 
-        mapview.getMap().getMapObjects().addTapListener(new MapObjectTapListener() {
-            @Override
-            public boolean onMapObjectTap(@NonNull MapObject mapObject, @NonNull Point point) {
-                Log.d("ccpp",point.toString());
-
-                return false;
-            }
-        });
-        final LocationManager locationManager = MapKitFactory.getInstance().createLocationManager();
+       /* LocationManager locationManager = MapKitFactory.getInstance().createLocationManager();
         locationManager.requestSingleUpdate(new LocationListener() {
             @Override
             public void onLocationUpdated(@NonNull Location location) {
@@ -104,20 +59,41 @@ public class MainActivity extends AppCompatActivity {
             public void onLocationStatusUpdated(@NonNull LocationStatus locationStatus) {
 
             }
-        });
+        })*/;
+     /*   db.collection("points")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("Firebase", document.getId() + " => " + document.getData().get("gp"));
+                                GeoPoint geoPoint = (GeoPoint) document.getData().get("gp");
+
+                                mapview.getMap().getMapObjects().addPlacemark(
+                                        new Point(geoPoint.getLatitude(),geoPoint.getLongitude()),
+                                        ImageProvider.fromBitmap(getBitmapFromVectorDrawable(getApplicationContext(),R.drawable.ic_person_pin)));
+                            }
+                        } else {
+                            Log.w("Firebase", "Error getting documents.", task.getException());
+                        }
+                    }
+                });*/
+
+
 
     }
     @Override
     protected void onStop() {
         super.onStop();
-        mapview.onStop();
+        mapView.onStop();
         MapKitFactory.getInstance().onStop();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mapview.onStart();
+        mapView.onStart();
         MapKitFactory.getInstance().onStart();
     }
     public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
@@ -137,4 +113,5 @@ public class MainActivity extends AppCompatActivity {
 
         return bitmap;
     }
-}
+
+ }
